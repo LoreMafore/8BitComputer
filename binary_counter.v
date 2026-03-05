e0e2f4789a593ed1a7ac4f2639783dfcf8fb2f17
Conrad Mercer <144570519+LoreMafore@users.noreply.github.com>
2026-03-04 22:27:39 -0600

    works now

--- binary_counter.v
+++ binary_counter.v
@@ -7,68 +7,87 @@ module binary_counter(
     input wire clear,
     input wire reset,
     output reg [3:0] leds
-    );
-	 
-    reg [3:0] counter;
-	 reg [3:0] bus_latch;
-	 reg is_counter_enabled;
-	 reg counter_enable_prev;
-	 
+);
+
+    localparam LOCKOUT_CYCLES = 27'd50_000_000;
+    localparam COUNT_CYCLES   = 27'd100_000_000;
+
+    reg [3:0]  counter;
+    reg [3:0]  bus_latch;
+    reg        is_counter_enabled;
+    reg        counter_enable_prev;
+    reg        counter_out_prev;
+    reg [26:0] enable_lockout_timer;
+    reg        in_lockout;
+    reg [26:0] count_timer;
+
     initial begin
-		  leds = 4'b0001;
-        counter = 4'b0001;
-		  bus_latch = 4'b0000;
-		  is_counter_enabled = 1'b0;
-		  counter_enable_prev = 1'b0;
+        leds                 = 4'b0001;
+        counter              = 4'b0001;
+        bus_latch            = 4'b0000;
+        is_counter_enabled   = 1'b0;
+        counter_enable_prev  = 1'b0;
+        counter_out_prev     = 1'b0;
+        enable_lockout_timer = 27'd0;
+        in_lockout           = 1'b0;
+        count_timer          = 27'd0;
     end
-	 
+
     assign bus[7:4] = 4'bz;
-	 assign bus[3:0] = bus_latch;
-	 
-	 always @(posedge clk)
-    begin
-        if (clear)
-            bus_latch <= 4'b0000;
-        else if (counter_out)
-            bus_latch <= counter;
-    end
-	 
-    always @(posedge clk)
-    begin
-        if (clear)
-        begin
-            is_counter_enabled <= 1'b0;
-            counter_enable_prev <= 1'b0;
-        end
-        else
-        begin
+    assign bus[3:0] = bus_latch;
+
+    always @(posedge clk or posedge clear) begin
+        if (clear) begin
+            counter              <= 4'b0000;
+            bus_latch            <= 4'b0000;
+            is_counter_enabled   <= 1'b0;
+            counter_enable_prev  <= 1'b0;
+            counter_out_prev     <= 1'b0;
+            in_lockout           <= 1'b0;
+            enable_lockout_timer <= 27'd0;
+            count_timer          <= 27'd0;
+        end else begin
+
             counter_enable_prev <= counter_enable;
-            if (counter_enable && !counter_enable_prev)
-                is_counter_enabled <= ~is_counter_enabled;
+            counter_out_prev    <= counter_out;
+
+            if (counter_out && !counter_out_prev)
+                bus_latch <= counter;
+
+            if (in_lockout) begin
+                if (enable_lockout_timer >= LOCKOUT_CYCLES - 1'b1) begin
+                    enable_lockout_timer <= 27'd0;
+                    in_lockout           <= 1'b0;
+                end else begin
+                    enable_lockout_timer <= enable_lockout_timer + 1'b1;
+                end
+            end else begin
+                if (counter_enable && !counter_enable_prev) begin
+                    is_counter_enabled   <= ~is_counter_enabled;
+                    in_lockout           <= 1'b1;
+                    enable_lockout_timer <= 27'd0;
+                end
+            end
+
+            if (counter_in) begin
+                counter     <= bus[3:0];
+                count_timer <= 27'd0;
+            end else if (is_counter_enabled) begin
+                if (count_timer >= COUNT_CYCLES - 1'b1) begin
+                    count_timer <= 27'd0;
+                    counter     <= counter + 1'b1;
+                end else begin
+                    count_timer <= count_timer + 1'b1;
+                end
+            end else begin
+                count_timer <= 27'd0;
+            end
+
         end
     end
 
-    always @(posedge clk or posedge clear)
-    begin
-        if (clear)
-        begin
-            counter <= 4'b0000;
-        end
-        else if (counter_in)
-        begin
-            counter <= bus[3:0];
-        end
-        else if (is_counter_enabled)
-        begin
-            counter <= counter + 1'b1;
-        end
-    end
-
-    // Continuous update for LEDs
-    always @(*)
-    begin 
+    always @(*) begin
         leds = counter;
     end
 
 endmodule
-
