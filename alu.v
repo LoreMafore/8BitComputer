d44deda2441e568af04489feb4227202c9e989f3
Conrad Mercer <144570519+LoreMafore@users.noreply.github.com>
2026-03-12 09:10:13 -0500

    I may be cooking the ALU

--- alu.v
+++ alu.v
@@ -1,6 +1,72 @@
 module alu_a_b(
     input clk,
     inout [7:0] bus,
-	 input wire clear,
+	input wire clear,
     input wire reset, 
+	inout [7:0] reg_a,
+	inout [7:0] reg_b,
+    output [7:0] alu_value,
+    input wire alu_flag,
+    input wire a_out_flag,
+    input wire a_in_flag,
+    input wire b_out_flag,
+    input wire b_in_flag,
+    input wire b_sub
 );
+
+
+reg a_in, b_in, alu_in;
+reg a_ack, b_ack, alu_ack;
+
+
+initial begin
+    reg_a = 8'b00001100;
+    reg_b = 8'b00100000;
+end
+
+
+always @(*) begin
+    
+    if (b_sub == 0 )begin
+        alu_value <= reg_a + reb_b;
+    end
+    
+    else begin
+        alu_value <= reg_a - reg_b;
+    end
+end
+
+
+always @(posedge a_in_flag or posedge a_ack) 
+    if(a_ack) a_out <= 1'b0; else a_out <= 1'b1;
+
+always @(posedge b_in_flag or posedge b_ack) 
+    if(b_ack) b_out <= 1'b0; else b_out <= 1'b1;
+
+always @(posedge a_out_flag)
+    bus <= reg_a;
+
+always @(posedge b_out_flag)
+    bus <= reg_b;
+
+always @(posedge alu_flag)
+    bus <= alu_value;
+
+always @(posedge clk or posedge clear or posedge reset)
+    if(clear) begin
+        reg_a <= 8'b00000000;
+    end
+    
+    else if(reset) begin
+        reg_b <= 8'b00000000;
+    end
+
+    else begin
+        {a_ack, b_ack, alu_ack} <= 3'b000;
+        
+        if (a_out_flag) begin
+            
+        end
+    end
+
+endmodule
--- computer.v
+++ computer.v
@@ -3,7 +3,10 @@ module computer(
     input CLK,
     input RESET,
     inout [7:0] eight_bit_bus,
-    output [3:0] LEDS 
+    output [3:0] LEDS, 
+	inout [7:0] REG_A,
+	inout [7:0] REG_B
+    output [7:0] ALU_VALUE, 
 );
 
 //Control Wires 
@@ -11,6 +14,12 @@ wire ENABLE_COUNTER;
 wire COUNTER_OUT;
 wire COUNTER_IN;
 wire COUNTER_CLEAR;
+wire ALU_FLAG;
+wire A_IN_FLAG;
+wire A_OUT_FLAG;
+wire B_IN_FLAG;
+wire B_OUT_FLAG;
+wire B_SUB;
 
 assign ENABLE_COUNTER = ~pc_drive[0];
 assign COUNTER_OUT    = ~pc_drive[1];
@@ -21,7 +30,7 @@ assign COUNTER_CLEAR  = ~pc_drive[3];
 //Program Counter
 binary_counter counter(
     .clk(CLK),
-	 .bus(eight_bit_bus),
+	.bus(eight_bit_bus),
     .counter_enable(ENABLE_COUNTER),
     .counter_out(COUNTER_OUT),
     .counter_in(COUNTER_IN),
@@ -36,7 +45,16 @@ alu alu_a_b(
 	.clk(CLK),
 	.bus(eight_bit_bus),
 	.clear(COUNTER_CLEAR),
-	.reset(RESET)
+	.reset(RESET),
+	.reg_a(REG_A),
+	.reg_b(REG_B),
+    .alu_value(ALU_VALUE),
+    .alu_flag(ALU_FLAG),
+    .a_flag(A_IN_FLAG),
+    .a_flag(A_OUT_FLAG),
+    .b_flag(B_IN_FLAG),
+    .b_flag(B_OUT_FLAG),
+    .b_sub(B_SUB)
 )
 
 
