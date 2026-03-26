2f2d20940a0955b4b813aef78745bede24dc299b
Conrad Mercer <144570519+LoreMafore@users.noreply.github.com>
2026-03-26 09:05:47 -0500

    Nothing ever happens

--- alu.v
+++ alu.v
@@ -1,6 +1,6 @@
 module alu(
     input clk,
-    inout wire[7:0] bus,
+    inout wire [7:0] bus,
     input wire clear,
     input wire reset, 
     input wire alu_flag,
@@ -11,15 +11,11 @@ module alu(
     input wire b_sub
 );
 
-reg a_in, b_in;
-reg a_ack, b_ack, alu_ack;
 reg [7:0] reg_a;
 reg [7:0] reg_b;
 reg [7:0] alu_value;
-
-assign bus = a_out_flag ? reg_a :
-             b_out_flag ? reg_b :
-             alu_flag   ? alu_value : 8'bz;
+reg [7:0] bus_latch;
+reg a_in, b_in, a_ack, b_ack, alu_ack;
 
 initial begin
     reg_a     = 8'b00001100;
@@ -27,17 +23,23 @@ initial begin
     alu_value = 8'b00000000;
 end
 
-
 always @(*) begin
-    if (b_sub == 0 )begin
-        alu_value <= reg_a + reg_b;
-    end
-    
-    else begin
-        alu_value <= reg_a - reg_b;
-    end
+    if (b_sub == 1'b0)
+        alu_value = reg_a + reg_b;
+    else
+        alu_value = reg_a - reg_b;
 end
 
+always @(*) begin
+    if (a_out_flag)
+        bus_latch = reg_a;
+    else if (b_out_flag)
+        bus_latch = reg_b;
+    else if (alu_flag)
+        bus_latch = 8'b00000001;
+end
+
+assign bus = (a_out_flag || b_out_flag || alu_flag) ? bus_latch : 8'bz;
 
 always @(posedge a_in_flag or posedge a_ack) 
     if(a_ack) a_in <= 1'b0; else a_in <= 1'b1;
@@ -50,20 +52,16 @@ always @(posedge clk or posedge clear or posedge reset) begin
         reg_a <= 8'b00000000;
         a_ack <= 1'b0;
     end
-    
     else if(reset) begin
         reg_b <= 8'b00000000;
         a_ack <= 1'b0;
     end
-
     else begin
         {a_ack, b_ack, alu_ack} <= 3'b000;
-        
         if (a_in) begin
-            reg_a <= bus; 
+            reg_a <= bus;
             a_ack <= 1'b1;
         end
-
         if (b_in) begin
             reg_b <= bus;
             b_ack <= 1'b1;
